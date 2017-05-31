7e9a1c2e030cc7225f011bff6e5ca350ea327fe8
Shibin Moideen <shibin.moideen@cabotsolutions.com>
2017-05-31 21:24:58 +0530

    Added collection view with static data.

---
+++ NYTimesClone/NYTimesClone/Classes/CustomView/CustomCells/NewsFeedCollectionViewCell.swift
@@ -0,0 +1,20 @@
+//
+//  NewsFeedCollectionViewCell.swift
+//  NYTimesClone
+//
+//  Created by Shibin Moideen on 5/31/17.
+//  Copyright © 2017 Shibin Moideen. All rights reserved.
+//
+
+import UIKit
+
+class NewsFeedCollectionViewCell: UICollectionViewCell {
+
+    @IBOutlet weak var newsTitleLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
---
+++ NYTimesClone/NYTimesClone/Classes/ViewControllers/Home/HomeViewController.swift
@@ -0,0 +1,90 @@
+//
+//  HomeViewController.swift
+//  NYTimesClone
+//
+//  Created by Shibin Moideen on 5/31/17.
+//  Copyright © 2017 Shibin Moideen. All rights reserved.
+//
+
+import UIKit
+
+private let reuseIdentifier = "NewsCollectionCell"
+
+class HomeViewController: UICollectionViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+
+        // Register custom cell classes
+        self.collectionView?.register(UINib.init(nibName: "NewsFeedCollectionViewCell", bundle: nil), forCellWithReuseIdentifier: reuseIdentifier)
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    // MARK: UICollectionViewDataSource
+
+    override func numberOfSections(in collectionView: UICollectionView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 1
+    }
+
+
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of items
+        return 20
+    }
+
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> NewsFeedCollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: reuseIdentifier, for: indexPath) as! NewsFeedCollectionViewCell
+    
+        // Configure the cell
+        cell.newsTitleLabel.text = "News Title \(indexPath.row)"
+        if indexPath.row % 2 == 0 {
+            cell.backgroundColor = UIColor.red
+        }
+        else {
+            cell.backgroundColor = UIColor.green
+        }
+        return cell
+    }
+
+    // MARK: UICollectionViewDelegate
+
+    /*
+    // Uncomment this method to specify if the specified item should be highlighted during tracking
+    override func collectionView(_ collectionView: UICollectionView, shouldHighlightItemAt indexPath: IndexPath) -> Bool {
+        return true
+    }
+    */
+
+    /*
+    // Uncomment this method to specify if the specified item should be selected
+    override func collectionView(_ collectionView: UICollectionView, shouldSelectItemAt indexPath: IndexPath) -> Bool {
+        return true
+    }
+    */
+
+    /*
+    // Uncomment these methods to specify if an action menu should be displayed for the specified item, and react to actions performed on the item
+    override func collectionView(_ collectionView: UICollectionView, shouldShowMenuForItemAt indexPath: IndexPath) -> Bool {
+        return false
+    }
+
+    override func collectionView(_ collectionView: UICollectionView, canPerformAction action: Selector, forItemAt indexPath: IndexPath, withSender sender: Any?) -> Bool {
+        return false
+    }
+
+    override func collectionView(_ collectionView: UICollectionView, performAction action: Selector, forItemAt indexPath: IndexPath, withSender sender: Any?) {
+    
+    }
+    */
+
+}
